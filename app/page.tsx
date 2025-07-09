f12fb0c7cf84e4e98ae5e6475faa016c4659b9f4
ruslanfin <ruslan@startupfinland.com>
2025-07-09 21:28:36 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,10 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+            We forked that boilerplate
           </li>
           <li className="tracking-[-.01em]">
-            Git push and see your changes instantly.
+            We deployed that in a minute with DollarDeploy
           </li>
         </ol>
 
@@ -39,7 +35,7 @@ export default function Home() {
               width={32}
               height={32}
             />
-            Deploy now
+            OK!
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
